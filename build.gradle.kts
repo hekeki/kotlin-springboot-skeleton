6429c4c851e8d9c0fa01ceaa329e68389858ed79
hekeki <github@hekeki.com>
2023-10-08 16:54:04 +0200

    Update Spring Boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 }
 
 plugins {
-    id("org.springframework.boot") version "3.1.3"
+    id("org.springframework.boot") version "3.1.4"
     id("io.spring.dependency-management") version "1.1.3"
     id("org.owasp.dependencycheck") version "8.4.0"
     id("org.jetbrains.kotlin.jvm") version "1.9.10"
