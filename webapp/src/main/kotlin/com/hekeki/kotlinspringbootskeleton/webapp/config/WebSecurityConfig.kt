661d154bb3b6499c1ed531456fbf198128e0dac8
hekeki <github@hekeki.com>
2024-05-01 12:44:45 +0200

    redirect logout to login page

--- webapp/src/main/kotlin/com/hekeki/kotlinspringbootskeleton/webapp/config/WebSecurityConfig.kt
+++ webapp/src/main/kotlin/com/hekeki/kotlinspringbootskeleton/webapp/config/WebSecurityConfig.kt
@@ -33,6 +33,9 @@ class WebSecurityConfig {
                 loginPage = "/login"
                 permitAll()
             }
+            logout {
+                logoutSuccessUrl = "/login"
+            }
         }
 
         return http.build()
